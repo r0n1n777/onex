fb62d9dc556ac9afd698371e0c5657c2a5d595f1
Ero Aguilar <eroaguilar7@gmail.com>
2021-03-28 16:32:15 +0800

    Added migrations to add admin column in users table.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,11 +13,16 @@ class ComposerAutoloaderInit0ea923a0f7195ca388bd90f088014db1
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
+
+        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit0ea923a0f7195ca388bd90f088014db1', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -25,7 +30,7 @@ class ComposerAutoloaderInit0ea923a0f7195ca388bd90f088014db1
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit0ea923a0f7195ca388bd90f088014db1::getInitializer($loader));
         } else {
